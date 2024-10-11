156b8a2bcd818a85b93e414ee1fd03ffdf7cb636
hector-elias <hectorelias@kms-technology.com>
2024-10-11 14:06:39 -0600

    Added new files.

---
+++ constants/SalesforceConstants.ts
@@ -0,0 +1,16 @@
+export const NavbarMenu = {
+    Home: 'Home',
+    Opportunities: 'Opportunities',
+    Leads: 'Leads',
+    Tasks: 'Tasks',
+    Files: 'Files',
+    Accounts: 'Accounts',
+    Contacts: 'Contacts',
+    Campaigns: 'Campaigns',
+    Dashboard: 'Dashboard',
+    Reports: 'Reports',
+    Chatter: 'Chatter',
+    Groups: 'Groups',
+    Calendar: 'Calendar',
+    More: 'More'
+};
