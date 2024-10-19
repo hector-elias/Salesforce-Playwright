c894a6e6f2e86a5af1a22a70b37bdc72da9aec78
hector-elias <hectorelias@kms-technology.com>
2024-10-19 15:11:40 -0600

    Removed methods for waiting an element.

--- actions/ElementActions.ts
+++ actions/ElementActions.ts
@@ -2,15 +2,13 @@ import { type Locator } from "@playwright/test";
 
 export class ElementActions {
 
-    async clickElement(element: Locator, timeout: number = 5000): Promise<void> {
+    async clickElement(element: Locator, timeout: number = 30000): Promise<void> {
         console.log(`Clicking on element: ${element.toString()}`);
-        await element.waitFor({ state: 'visible', timeout });
-        await element.click();
+        await element.click({ timeout });
     }
 
-    async enterText(element: Locator, text: string, timeout: number = 5000): Promise<void> {
+    async enterText(element: Locator, text: string, timeout: number = 30000): Promise<void> {
         console.log(`Entering text: "${text}" into element: ${element.toString()}`);
-        await element.waitFor({ state: 'visible', timeout });
-        await element.fill(text);
+        await element.fill(text, { timeout });
     }
 }
