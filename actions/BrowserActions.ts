0ea3c3769cf93b2db54f255f93bbb498f9570093
hector-elias <hectorelias@kms-technology.com>
2024-10-19 15:10:28 -0600

    Minor changes.

--- actions/BrowserActions.ts
+++ actions/BrowserActions.ts
@@ -3,6 +3,6 @@ import { type Page } from '@playwright/test';
 export class BrowserActions {
 
     async navigateToUrl(page: Page, url: string): Promise<void> {
-        await page.goto(url, { waitUntil: 'load' });
+        await page.goto(url, { waitUntil: 'domcontentloaded' });
     }
 }
