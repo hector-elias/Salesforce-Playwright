d2309246eb44f4c0fb15ca57ff0c15d37d410d5a
hector-elias <hectorelias@kms-technology.com>
2024-10-25 09:37:42 -0600

    Added closePage method.

--- actions/BrowserActions.ts
+++ actions/BrowserActions.ts
@@ -5,4 +5,8 @@ export class BrowserActions {
     async navigateToUrl(page: Page, url: string): Promise<void> {
         await page.goto(url, { waitUntil: 'domcontentloaded' });
     }
+
+    async closePage(page: Page): Promise<void> {
+        await page.close();
+    }
 }
