ee773312758a6a56b095b16577c7d97eebb2487c
hector-elias <hectorelias@kms-technology.com>
2024-10-25 09:38:10 -0600

    Enhancing methods

--- actions/ElementActions.ts
+++ actions/ElementActions.ts
@@ -2,13 +2,44 @@ import { type Locator } from "@playwright/test";
 
 export class ElementActions {
 
-    async clickElement(element: Locator, timeout: number = 30000): Promise<void> {
-        console.log(`Clicking on element: ${element.toString()}`);
-        await element.click({ timeout });
+    async clickElement(element: Locator, timeout?: number): Promise<void> {
+        try {
+            console.log(`Waiting for element to be visible and present in DOM: ${element.toString()} - ${timeout || 'default (30s)'}`);
+            await element.waitFor({ state: 'visible', timeout });
+            await element.waitFor({ state: 'attached', timeout });
+
+            const isEnabled = await element.isEnabled();
+
+            if (!isEnabled) {
+                throw new Error(`The ${element.toString()} isn't enabled.`);
+            }
+
+            console.log(`Clicking on element: ${element.toString()}`);
+            await element.click({ timeout, force: true });
+
+        } catch (error) {
+            console.error(`Failed to click on element: ${element.toString()}. Error: ${error}`);
+            throw error;
+        }
     }
 
-    async enterText(element: Locator, text: string, timeout: number = 30000): Promise<void> {
-        console.log(`Entering text: "${text}" into element: ${element.toString()}`);
-        await element.fill(text, { timeout });
+    async enterText(element: Locator, text: string, timeout?: number): Promise<void> {
+        try {
+            console.log(`Waiting for element to be visible and present in DOM: ${element.toString()} - ${timeout || 'default (30s)'}`);
+            await element.waitFor({ state: 'visible', timeout });
+            await element.waitFor({ state: 'attached', timeout });
+
+            const isEnabled = await element.isEnabled();
+            if (!isEnabled) {
+                throw new Error(`The ${element.toString()} isn't enabled.`);
+            }
+
+            console.log(`Entering text: "${text}" into element: ${element.toString()}`);
+            await element.fill(text, { timeout });
+
+        } catch (error) {
+            console.error(`Failed to enter text into element: ${element.toString()}. Error: ${error}`);
+            throw error;
+        }
     }
 }
